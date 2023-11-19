6bb3b7fd9dc6cf148c56f2fbfca205cbc9577ccb
terence planchestainer <t.planchestainer@stag.technofuturtic.education>
2023-11-19 20:16:29 +0100

    pok

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+// 2 Importer les dependances 
+import { FormGroup,FormBuilder,Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,44 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'formulaireReactif';
+  //3 FormGroup declaration
+  monFormGroup! : FormGroup
+oui:boolean =false
+nomValues! : string 
+nomValue! : string 
+laDate! : Date
+
+//4 constructor pour le FormBuilder 
+constructor(private _monFormBuilder : FormBuilder){
+  this.monFormGroup = this._monFormBuilder.group({
+    nom :["",Validators.required],
+    email : ["",Validators.required],
+    date:["",Validators.required]
+  })
+}
+// recuperer la valeur de mes input 
+methodeSubmit(){
+  // si il est valide 
+  if (this.monFormGroup.valid) {
+    //recuperer l input en question
+   this.nomValue = this.monFormGroup.get('nom')?.value
+    this.oui = true
+// recuperer un objet
+    this.nomValues = this.monFormGroup.value
+    
+    console.log(this.nomValue);
+    console.log(this.nomValues);
+    
+    this.laDate = this.monFormGroup.get('date')?.value
+    
+   
+    
+  }else{
+    console.log("fonctionne PAS!!!!");
+    this.oui = false
+    
+  }
+  
+}
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,19 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+ // 1 rajouter la dependance elle se rajoute normalement par autocompletion
+import { ReactiveFormsModule } from '@angular/forms';
+
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    // 1 rajouter la dependance
+    ReactiveFormsModule
+  
   ],
   providers: [],
   bootstrap: [AppComponent]
